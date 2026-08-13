e03385818b965a6c89cad7f71b547ec94efc4e41
Teemo Tebest <teemo.tebest@gmail.com>
2026-08-14 00:56:50 +0300

    Host UNCTAD logo locally instead of hardcoded unctad.org URL (blocks downloads on localhost)

--- src/jsx/components/ChartStackedColumn.jsx
+++ src/jsx/components/ChartStackedColumn.jsx
@@ -70,7 +70,7 @@ function ColumnChart({
         events: {
           load() {
             // eslint-disable-next-line react/no-this-in-sfc
-            this.renderer.image('https://unctad.org/sites/default/files/2022-11/unctad_logo.svg', 5, 15, 80, 100).add();
+            this.renderer.image(`${window.location.href.includes('unctad.org') ? 'https://storage.unctad.org/2022-rmt_report/' : './'}assets/img/unctad_logo.svg`, 5, 15, 80, 100).add();
           }
         },
         height: chartHeight,
--- src/jsx/components/Video.jsx
+++ src/jsx/components/Video.jsx
@@ -52,7 +52,7 @@ function Video({ anchorClick }) {
           <div>stormy waters</div>
         </h4>
         <h4 className="unctad_logo">
-          <img src="https://unctad.org/sites/default/files/2022-11/unctad_logo_white.svg" alt="UNCTAD logo" />
+          <img src={`${window.location.href.includes('unctad.org') ? 'https://storage.unctad.org/2022-rmt_report/' : './'}assets/img/unctad_logo_white.svg`} alt="UNCTAD logo" />
         </h4>
         <svg className="arrows" onClick={() => anchorClick('.pagenavigation_container', 'Arrows')}>
           <path className="a1" d="M0 0 L30 32 L60 0" />
